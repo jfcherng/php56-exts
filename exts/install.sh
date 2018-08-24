8d5482cec9f0caed0611b6d06deab63912318a6f
Jack Cherng <jfcherng@gmail.com>
2018-08-25 06:28:58 +0800

    Add install.sh
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ exts/install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+THREAD_CNT=$(nproc --all)
+
+
+#----------------#
+# configurations #
+#----------------#
+
+php_install_dir=/usr/local/php56
+
+
+#----------------------#
+# environment checking #
+#----------------------#
+
+PHPIZE="${php_install_dir}/bin/phpize"
+PHP_CONFIG="${php_install_dir}/bin/php-config"
+
+test -f "${PHPIZE}"     || (echo "${PHPIZE} not found"     && exit)
+test -f "${PHP_CONFIG}" || (echo "${PHP_CONFIG} not found" && exit)
+
+
+#-------#
+# begin #
+#-------#
+
+pushd "${SCRIPT_DIR}" || exit
+
+for ext_dir in */; do
+    pushd "${ext_dir}" || exit
+
+    ${PHPIZE}
+    ./configure --with-php-config="${PHP_CONFIG}"
+    make -j"${THREAD_CNT}" && make install && make clean
+
+    popd || exit
+done
+
+popd || exit
